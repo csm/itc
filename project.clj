95f2837e0947752ed218e8888dc17d2da742f91c
Casey Marshall <csm@noonhome.com>
2019-11-21 16:47:44 -0800

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.csm/itc "0.1.0-SNAPSHOT"
+(defproject com.github.csm/itc "0.1.0"
   :description "Interval tree clocks for clojure/clojurescript"
   :url "https://github.com/csm/itc"
   :license {:name "LGPLv3"
